0a8f9ff172dd9fdb1a082b9b24a32712d814220a
Toureiro2 <joao_toureiro@hotmail.com>
2024-09-05 12:22:16 -0300

    Resolução da questão 5 - Inversor de caracteres

--- src/main/java/questoes/Questao5.java
+++ src/main/java/questoes/Questao5.java
@@ -1,9 +1,23 @@
 package questoes;
 
+import java.util.Scanner;
+
 public class Questao5 {
 
-	public Questao5() {
-		// TODO Auto-generated constructor stub
-	}
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma string: ");
+        
+        scanner.close();
+        
+        String original = scanner.nextLine();
+        
+        String invertida = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            invertida += original.charAt(i);
+        }
+        System.out.println("String invertida: " + invertida);
+    }
 
 }
